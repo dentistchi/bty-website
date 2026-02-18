55570f2dc595b03b0b6f530a598034cb782d6a7f
Hanbit Chi <hanbit@Hanbits-MacBook-Air.local>
2026-02-18 13:57:33 -0800

    fix: import useEffect in CoachChatPane

--- bty-app/src/components/train/CoachChatPane.tsx
+++ bty-app/src/components/train/CoachChatPane.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useMemo, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import { usePathname } from "next/navigation";
 import { getTrainDay } from "@/lib/trainContent";
 
